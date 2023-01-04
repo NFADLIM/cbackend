6d4362dd6f2d848456db9c125b54f7c0f90daa79
NFADLIM <hamdibgr@gmail.com>
2023-01-04 17:45:42 +0700

    update

--- index.js
+++ index.js
@@ -5,20 +5,15 @@ const cors = require('cors');
 const cookieParser = require("cookie-parser");
 const sessions = require('express-session');
 const app = express();
-
+const path = require('path');
 const fasilitasroute = require ('./fasilitasroute');
 const gedungroute = require ('./gedungroute');
-const gambarroute = require ('./gambarroute');
+
 
 
 const oneDay = 1000 * 60 * 60 * 24;
 
-app.use(sessions({
-  secret: "tokenkey123445",
-  saveUninitialized:true,
-  cookie: { maxAge: oneDay },
-  resave: false
-}));
+
 
 app.use(cors());
 app.use(express.json());
@@ -26,55 +21,55 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname));
 app.use(cookieParser());
 
-const myusername = 'Thisadmin1'
-const mypass = 'admin12345'
-
-var session;
-
-app.get('/',(req,res) => {
-  session=req.session;
-  if(session.userid){
-      res.send("Welcome User ");
-  }else
-  res.sendFile('./home.html',{root:__dirname})
-
-});
-
-app.post('/login',(req,res) => {
-  if(req.body.username == myusername && req.body.password == mypass){
-      session=req.session;
-      session.userid=req.body.username;
-      console.log(req.session)
-      res.send(`Hey there, welcome <a href=\'/logout'>click to logout</a>`);
-      app.use(fasilitasroute);
-      app.use(gedungroute);
-  
-  }
-  else{
-      res.send('Invalid username or password');
-  }
-});
-
-app.get('/edit',(req,res) => {
-  session=req.session;
-  if(session.userid){
-    app.use(fasilitasroute);
-    app.use(gedungroute);
-
-  }else
-  res.sendFile('./home.html',{root:__dirname})
-
-});
-
 app.use(fasilitasroute);
 app.use(gedungroute);
-//app.use(gambarroute);
 
-app.get('/logout',(req,res) => {
-  req.session.destroy();
-  res.redirect('/');
+app.use(sessions({
+  secret: "tokenkey123445",
+  saveUninitialized:true,
+  cookie: { maxAge: oneDay },
+  resave: false
+}));
+app.get('/', function(request, response) {
+	response.sendFile(path.join(__dirname + '/login.html'));
 });
+
+app.post('/auth', function(request, response) {
+	let username = request.body.username;
+	let password = request.body.password;
+	if (username && password) {
+		db.query('SELECT * FROM users WHERE username = ? AND password = ?', [username, password], function(error, results, fields) {
+			if (error) throw error;
+			if (results.length > 0) {
+				request.session.loggedin = true;
+				request.session.username = username;
+				response.redirect('/edit');
+			} else {
+				response.send('Incorrect Username and/or Password!');
+			}			
+			response.end();
+		});
+	} else {
+		response.send('Please enter Username and Password!');
+		response.end();
+	}
+});
+
+app.get('/edit',(request,response) => {
+if (request.session.loggedin) {
+  response.send('Welcome back, ' + request.session.username + '!');
+app.use(fasilitasroute);
+app.use(gedungroute);
+
+} else {
+  response.send('Please login to view this page!');
+}
+response.end();
+
+});
+
+
 app.listen(3000, () => {
     console.log('server berhasil berjalan pada port 3000!');
   });
- 
+ //app.use(gambarroute);
