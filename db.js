4144f5ddf0ba76825e46a75bc36b5d6b2d3adee0
NFADLIM <hamdibgr@gmail.com>
2023-01-09 15:51:46 +0700

    deploying

--- db.js
+++ db.js
@@ -2,10 +2,10 @@ const mysql = require('mysql');
 
 const db = mysql.createPool({
 
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'vtour'
+  host: process.env.DB_HOST,
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DBNAME
 });
 
 exports.db = db;
